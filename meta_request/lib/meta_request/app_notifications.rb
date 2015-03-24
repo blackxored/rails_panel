741def0be68d0d5bf98d13cc4f9d8bc7285bb11e
Adrian Perez <adrianperez.deb@gmail.com>
2015-03-24 22:15:00 +0100

    Add bullet events to meta_request

--- meta_request/lib/meta_request/app_notifications.rb
+++ meta_request/lib/meta_request/app_notifications.rb
@@ -23,6 +23,21 @@ module MetaRequest
           payload[:format] ||= (payload[:formats]||[]).first # Rails 3.0.x Support
           payload[:status] = '500' if payload[:exception]
           Event.new(name, start, ending, transaction_id, payload)
+        end.
+        subscribe("bullet") do |*args|
+          name, start, ending, transaction_id, payload = args
+          new_payload = {}
+          class_mappings = {
+            'NPlusOneQuery' => 'N+1'
+          }
+
+          detection = payload[:detection]
+
+          new_payload[:type] = class_mappings[detection.class.name.demodulize]
+          new_payload[:location] = detection.instance_variable_get(:@callers).first.gsub(/:in(.+)$/, '').gsub(MetaRequest.rails_root + '/', '')
+          new_payload[:base_class] = detection.instance_variable_get(:@base_class)
+          new_payload[:associations] = detection.instance_variable_get(:@associations)
+          event = Event.new(name, start, ending, transaction_id, new_payload)
         end
     end
 
@@ -34,6 +49,8 @@ module MetaRequest
       self
     end
 
+    def process_bullet_detection(detection)
+    end
   end
 
 end
